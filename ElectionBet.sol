5585d4d3199f4da49134a42164e7a3a5f245dc76
Patrick Dunstone <patrick.dunstone@gmail.com>
2023-06-07 17:56:35 -0700

    integrate Oracle and Bet

--- ElectionBet.sol
+++ ElectionBet.sol
@@ -6,6 +6,12 @@ contract ElectionBet {
     ElectionResult public result = ElectionResult.Undecided;
     mapping(address => ElectionResult) public bets;
 
+    ElectionOracle oracle;
+
+    constructor(ElectionOracle _oracle) public {
+        oracle = _oracle;
+    }
+
     function placeBet(ElectionResult _bet) public {
         require(result == ElectionResult.Undecided, "Betting is closed");
         bets[msg.sender] = _bet;
@@ -13,6 +19,15 @@ contract ElectionBet {
 
     function resolve(ElectionResult _result) public {
         result = _result;
+    }
+
+    function updateResult(uint256 _result) public {
+        require(msg.sender == address(oracle), "Only the oracle can update the result");
+        if(_result == 0) {
+            resolve(ElectionResult.Candidate1);
+        } else if(_result == 1) {
+            resolve(ElectionResult.Candidate2);
+        }
     }
 
     function claim() public {
--- ElectionOracle.sol
+++ ElectionOracle.sol
@@ -1,52 +1,46 @@
-ElectionOracle.sol
-
 pragma solidity ^0.6.7;
 
 import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
 
 contract ElectionOracle is ChainlinkClient {
+    uint256 public electionResult;
+
     address private oracle;
     bytes32 private jobId;
     uint256 private fee;
 
-    string public electionData;
+    event ElectionResult(uint256 electionId, uint256 result);
 
-    constructor(address _oracle, string memory _jobId, uint256 _fee) public {
-        setPublicChainlinkToken();
+    constructor(address _oracle, string memory _jobId, uint256 _fee, address _link) public {
+        if (_link == address(0)) {
+            setPublicChainlinkToken();
+        } else {
+            setChainlinkToken(_link);
+        }
+
         oracle = _oracle;
         jobId = stringToBytes32(_jobId);
         fee = _fee;
     }
 
-    function requestElectionData(string memory _electionId) public returns (bytes32 requestId) {
+    function requestElectionResult(uint256 _electionId) public returns (bytes32 requestId) {
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
-        request.add("get", string(abi.encodePacked("https://api.ap.org/v2/elections/", _electionId)));
-        request.add("path", "results");
+        request.add("electionId", _electionId);
         return sendChainlinkRequestTo(oracle, request, fee);
     }
 
-    function fulfill(bytes32 _requestId, bytes32 _electionData) public recordChainlinkFulfillment(_requestId) {
-        electionData = bytes32ToString(_electionData);
+    function fulfill(bytes32 _requestId, uint256 _result) public recordChainlinkFulfillment(_requestId) {
+        electionResult = _result;
+        emit ElectionResult(electionId, _result);
     }
 
-    function bytes32ToString(bytes32 _bytes32) internal pure returns (string memory) {
-        uint8 i = 0;
-        while(i < 32 && _bytes32[i] != 0) {
-            i++;
-        }
-        bytes memory bytesArray = new bytes(i);
-        for(i = 0; i < 32 && _bytes32[i] != 0; i++) {
-            bytesArray[i] = _bytes32[i];
-        }
-        return string(bytesArray);
-    }
-
-    function stringToBytes32(string memory source) internal pure returns (bytes32 result) {
+    function stringToBytes32(string memory source) private pure returns (bytes32 result) {
         bytes memory tempEmptyStringTest = bytes(source);
         if (tempEmptyStringTest.length == 0) {
             return 0x0;
         }
-        assembly { // solhint-disable-line no-inline-assembly
+
+        assembly {
             result := mload(add(source, 32))
         }
     }
