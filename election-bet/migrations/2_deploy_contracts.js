8230118147901f863f32505ed2e390cb07562390
Patrick Dunstone <patrick.dunstone@gmail.com>
2023-06-08 17:07:09 -0700

    migration update 42069

--- election-bet/migrations/2_deploy_contracts.js
+++ election-bet/migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
 const ElectionBetMarket = artifacts.require('ElectionBetMarket');
 
 module.exports = function(deployer) {
-  const oracleAddress = 0xE176672151FFF04d7556752a6660822D3684BEAa; // Replace with the actual oracle address
-  const jobId = web3.utils.utf8ToHex(0x3231306231343334633035363464366662616662626431633462623234633665); // Replace with the actual job ID
+  const oracleAddress = '0xE176672151FFF04d7556752a6660822D3684BEAa'; // Enclose the address in quotes
+  const jobId = web3.utils.utf8ToHex('0x3231306231343334633035363464366662616662626431633462623234633665'); // Enclose the job ID in quotes
   const feeAmount = 100000000000000000; // Replace with the actual fee amount in LINK tokens
-  const tokenAddress = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB; // Replace with the actual token address
+  const tokenAddress = '0x326C977E6efc84E512bB9C30f76E30c160eD06FB'; // Enclose the address in quotes
 
   deployer.deploy(ElectionBetMarket, oracleAddress, jobId, feeAmount, tokenAddress);
 };
