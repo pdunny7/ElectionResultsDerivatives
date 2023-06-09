1dc6650307447f869638a8981383eddb6dce663b
Patrick Dunstone <patrick.dunstone@gmail.com>
2023-06-08 17:00:23 -0700

    redeploy params

--- election-bet/migrations/2_deploy_contracts.js
+++ election-bet/migrations/2_deploy_contracts.js
@@ -1,10 +1,13 @@
-const ElectionBetMarket = artifacts.require("ElectionBetMarket");
+const ElectionBetMarket = artifacts.require('ElectionBetMarket');
 
-module.exports = function (deployer) {
-  const oracleAddress = "0xE176672151FFF04d7556752a6660822D3684BEAa"; // Replace with the actual oracle address
-  const jobId = "0x6266303765656464616237373430363439616339613234303934346236316630"; // Replace with the actual job ID
-  const fee = 1 LINK; // Replace with the actual fee amount in ether
-  const tokenAddress = "0x514910771af9ca656af840dff83e8264ecf986ca"; // Replace with the actual token address
+module.exports = function(deployer) {
+  const oracleAddress = 0xE176672151FFF04d7556752a6660822D3684BEAa; // Replace with the actual oracle address
+  const jobId = web3.utils.utf8ToHex(0x3231306231343334633035363464366662616662626431633462623234633665); // Replace with the actual job ID
+  const feeAmount = 0.1; // Replace with the actual fee amount in LINK tokens
+  const tokenAddress = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB; // Replace with the actual token address
+
+  // Convert the fee amount to wei (smallest unit of LINK token)
+  const fee = web3.utils.toBN(feeAmount).mul(web3.utils.toBN(10).pow(web3.utils.toBN(18))); // Assuming LINK has 18 decimals
 
   deployer.deploy(ElectionBetMarket, oracleAddress, jobId, fee, tokenAddress);
 };
